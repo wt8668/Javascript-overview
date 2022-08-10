8158144fd7e011649d5d074eaf9d799f223ec0ea
wt8668 <110592829+wt8668@users.noreply.github.com>
2022-08-10 15:35:49 +0700

    Add files via upload

---
+++ using-operators.js
@@ -0,0 +1,11 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("Enter width")
+inputHeight = prompt("Enter height")
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+let area = width * height;
+document.write("The area is: " + area);
