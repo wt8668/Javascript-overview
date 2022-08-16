dfefa5e506f0902a9b2f86cc2af2412296a94c8b
wt8668 <110592829+wt8668@users.noreply.github.com>
2022-08-16 11:28:52 +0700

    Add files via upload

---
+++ declare-variable-sample.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 20
+let x = true;
+let y = 'Hanoi';
+
+document.write('a = ' + a);
+document.write('<br/>');
+document.write('b = ' + b);
+document.write('<br/>');
+document.write('x = ' + x);
+document.write('<br/>');
+document.write('y = ' + y);
+
+let width = 20;
+let height = 10;
+let area = width * height;
+document.write('<br/>');
+document.write('Area = ' + area);
